3af35ad34720cad431d36c50382e51e830076de1
Aman Yadav <amansy04@gmail.com>
2020-10-15 16:08:50 +0530

    Extraction and Files creation
    Script is able to extract data from website and make respective folders

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# codeforces-helper
+# Helper bot for codeforces contests
+
+### This script automatically extracts the sample test cases of a given codeforces contest, creates sperate files for input and output and organise them in sepearte folder.
+
+You can also provide your own template file and the script will create a solution file based on the template file for each question.
+
+## Installation
+- Linux
+    ```
+    pip3 install -r requirements.txt
+    python3 app.py <contest ID or contest URL>
+    ```
+- Windows 
+    ```
+    pip install -r requirements.txt
+    python app.py <contest ID or contest URL>
+    ```
+- examples
+    ```
+    python app.py 1234
+    ```
+    ```
+    python3 app.py https://codeforces.com/contest/1234
+    ```
---
+++ app.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+#importing libraries
+import urllib.request
+import sys
+from bs4 import BeautifulSoup
+import re
+import os
+from shutil import copyfile
+
+# Get data from url
+def get_page_data(url):
+    try:
+        url_contents = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(url_contents,"html.parser") 
+        return soup
+    except:
+        print("Unable to connect to the site. Please check your internet connection")
+        sys.exit(1)
+
+#Extract links of questions
+def get_question_links(soup):
+    links = []
+    pattern = r"/contest/\d+(/problem/.)"
+    for link in soup.findAll('a'):
+        res = re.search(pattern,str(link))
+        if res and res[1] not in links:
+            links.append(res[1])
+    return links
+
+# extract sample test cases from webpage
+def get_sample_test(soup):
+    sample_tests = soup.find("div",{"class": "sample-test"})
+    if sample_tests:
+        sample_tests = sample_tests.get_text(separator="\n")
+    sample_tests = re.sub(r'Input\n\n','Input\n',sample_tests)
+    sample_tests = re.sub(r'Output\n\n','Output\n',sample_tests)
+    return sample_tests.strip()
+
+# create files and directories of the given competition
+def create_files(problem_question_dir,problem_name,test_cases):
+    file_num_input = 1
+    file_num_output = 1
+    file_data = ""
+
+    test_split = test_cases.split('\n')
+    problem_dir = os.path.join(problem_question_dir,problem_name)
+    try:
+        os.mkdir(problem_dir)
+    except:
+        print("Directory",problem_name,"already exists..\nPerforming following tasks in",problem_name)
+    
+    if not os.path.exists(os.path.join(problem_dir,'solution.cpp')):
+        print('Copying C++ template file to',problem_dir)
+        try :
+            copyfile('template.cpp',os.path.join(problem_dir,'solution.cpp'))
+        except:
+            print("No template file found")
+    else:
+        print("Solution file already exists")
+
+    i=0
+    while i < len(test_split):
+        if test_split[i] == 'Input':
+            file_name = os.path.join(problem_dir,'input'+str(file_num_input)+'.txt')
+            i+=1
+            file_num_input+=1
+
+            while test_split[i] != 'Output':
+                file_data += str(test_split[i])+'\n'
+                i+=1
+
+            with open(file_name,'w') as file:
+                print('Writing',file_name)
+                file.write(file_data)
+                file_data = ""
+
+        if test_split[i] == 'Output':
+            file_name = os.path.join(problem_dir,'output'+str(file_num_output)+'.txt')
+            i+=1
+            file_num_output+=1
+
+            while i < len(test_split) and test_split[i] != 'Input':
+                file_data += str(test_split[i])+'\n'
+                i+=1
+
+            with open(file_name,'w') as file:
+                print('Writing',file_name)
+                file.write(file_data)
+                file_data = ""
+    print("File creation for problem",problem_dir,"completed..\n")
+
+# driver function
+if __name__ == '__main__':
+    try:
+        contest_id = sys.argv[1]
+    except:
+        print("Please provide contest ID or URL of codeforces contest as command line argument")
+        sys.exit(1)
+
+    if 'https://codeforces.com/contest/' in contest_id:
+        url = contest_id
+    else:
+        url = 'https://codeforces.com/contest/'+str(contest_id)
+    
+    html_data = get_page_data(url)
+    question_links = get_question_links(html_data)
+
+    problem_question_dir = re.search(r'\d+',url)[0]
+    
+    try:
+        os.mkdir(problem_question_dir)
+    except:
+        print("Directory",problem_question_dir,"already exists..\nPerforming following tasks in",problem_question_dir,"\n")
+
+    for link in question_links:
+        question_page_data = get_page_data(url+link)
+        test_cases = get_sample_test(question_page_data)
+        create_files(problem_question_dir,link[-1],test_cases)
